e4be9c401b1469d4b93f26a499409acaadaeb51c
Karl <karlgoddard0013@gmail.com>
2016-11-09 01:17:10 +0000

    Look up the word definition

---
+++ Lab 6/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    
+    $("#search").click(function(){
+        $.ajax({
+            type: "GET",
+            url: "request.php",
+            datatype: "html",
+            data: {q: "definition"},
+            success: function(text){
+                alert(text)
+            }
+        });
+    });
+});
